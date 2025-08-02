0459b91e1f2b6c5cc17b851f0685dc42f26a6ea4
qiguoyin81 <qiguoyin81@163.com>
2025-08-02 15:09:07 +0800

    feat: update components

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+// 图片资源导出文件
+const images = {
+  logo: 'https://www.weavefox.cn/api/bolt/unsplash_image?keyword=logo&width=200&height=60&random=logo_200_60',
+  loginBackground: 'https://www.weavefox.cn/api/bolt/unsplash_image?keyword=office&width=1200&height=800&random=office_1200_800',
+  userAvatar: 'https://www.weavefox.cn/api/bolt/unsplash_image?keyword=avatar&width=100&height=100&random=avatar_100_100',
+  companyLogo: 'https://www.weavefox.cn/api/bolt/unsplash_image?keyword=company&width=200&height=60&random=company_200_60'
+};
+
+export default images;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ConfigProvider } from 'antd';
-import App from './App';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import ManufacturingOrder from './modules/mrp/pages/ManufacturingOrder';
+import Login from './modules/auth/pages/Login';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConfigProvider theme={{ token: { colorPrimary: '#1890ff' } }}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ManufacturingOrder />} />
+        </Routes>
+      </Router>
     </ConfigProvider>
   </React.StrictMode>
 );
